3ffd4d5c76a9725ce74d6c6f01b6cdfd3ec34fc1
rahul-rathore786 <rahulrathorese786@gmail.com>
2025-07-13 17:19:32 +0530

    update to see account info deployer

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,38 +1,38 @@
-const { ethers } = require('hardhat')
-const fs = require('fs')
+const { ethers } = require("hardhat");
+const fs = require("fs");
 
 async function main() {
+  // get account
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with account:", deployer.address);
+  // get balance
+  console.log("Account balance:", (await deployer.getBalance()).toString());
   // Deploy USDT contract
-  const USDT = await ethers.getContractFactory('USDT')
-  const usdt = await USDT.deploy()
-  await usdt.deployed()
+  const USDT = await ethers.getContractFactory("USDT");
+  const usdt = await USDT.deploy();
+  await usdt.deployed();
 
   // Deploy DappWorks contract
-  const DappWorks = await ethers.getContractFactory('DappWorks')
-  const dappworks = await DappWorks.deploy(usdt.address)
-  await dappworks.deployed()
+  const DappWorks = await ethers.getContractFactory("DappWorks");
+  const dappworks = await DappWorks.deploy(usdt.address);
+  await dappworks.deployed();
 
   const addresses = JSON.stringify(
     { DappWorks: dappworks.address, USDT: usdt.address },
     null,
     4
-  )
+  );
 
-  fs.writeFile(
-    './src/abis/contractAddress.json',
-    addresses,
-    'utf8',
-    (err) => {
-      if (err) {
-        console.error(err)
-        return
-      }
-      console.log('Deployed contract addresses', addresses)
+  fs.writeFile("./src/abis/contractAddress.json", addresses, "utf8", (err) => {
+    if (err) {
+      console.error(err);
+      return;
     }
-  )
+    console.log("Deployed contract addresses", addresses);
+  });
 }
 
 main().catch((error) => {
-  console.error(error)
-  process.exitCode = 1
-})
+  console.error(error);
+  process.exitCode = 1;
+});
